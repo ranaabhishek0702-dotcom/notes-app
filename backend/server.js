96e1fff082777056d6092a089039ba1c05b60b3d
Abhishek Rana <abhishekrana@Abhisheks-MacBook-Pro-6.local>
2026-03-25 12:08:10 +0530

    MongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const connectDB = require('/Users/abhishekrana/notes-app/backend/config/db.js')
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
